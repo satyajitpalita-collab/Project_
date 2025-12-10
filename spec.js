124e7b0458869cd55d32599708f0270afc6982b4
satyajitpalita-collab <satyajitpalita000@gmail.com>
2025-12-10 17:09:13 +0530

    Create spec.js

---
+++ spec.js
@@ -0,0 +1,69 @@
+{
+    "title": "PMPM Values by Accountable Function (Highest to Lowest)",
+    "mark": "bar",
+    "encoding": {
+        "x": {
+            "field": "PMPM",
+            "title": "PMPM",
+            "type": "quantitative",
+            "sort": null,
+            "axis": {
+                "labelSeparation": 25
+            }
+        },
+        "y": {
+            "field": "Accountable Function",
+            "title": "Accountable Function",
+            "type": "nominal",
+            "sort": "-x"
+        }
+    },
+    "data": {
+        "values": [
+            {
+                "Accountable Function": "Payment Integrity",
+                "PMPM": 14.35
+            },
+            {
+                "Accountable Function": "Pharmacy",
+                "PMPM": 4.55
+            },
+            {
+                "Accountable Function": "Network",
+                "PMPM": 2.5
+            },
+            {
+                "Accountable Function": "Carelon Behavioral Health",
+                "PMPM": 1.08
+            },
+            {
+                "Accountable Function": "Medical Management",
+                "PMPM": 0.83
+            },
+            {
+                "Accountable Function": "Carelon Care Navigation",
+                "PMPM": 0.05
+            },
+            {
+                "Accountable Function": "Carelon Specialty Care Solutions",
+                "PMPM": 0.02
+            },
+            {
+                "Accountable Function": "Late Entry",
+                "PMPM": 0.01
+            },
+            {
+                "Accountable Function": "Stop Loss",
+                "PMPM": 0
+            },
+            {
+                "Accountable Function": "Carelon Palliative",
+                "PMPM": 0
+            },
+            {
+                "Accountable Function": "Carelon Post Acute Solutions",
+                "PMPM": -0.06
+            }
+        ]
+    }
+}
